bc3dec8e247695e5ef7cdb748ab921e278f97504
Grace Youpele <youpeleg@gmail.com>
2022-09-05 11:06:41 +0100

    app directory

---
+++ lib/pages/home/home.dart
@@ -0,0 +1,21 @@
+import 'package:ccn_application/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+      ),
+      body: const Center(
+        child: Text(
+          "Grace Youpele",
+          style: TextStyle(color: AppColor.primaryColor),
+        ),
+      ),
+    );
+  }
+}
